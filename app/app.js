85b94ab3689e5474be0814b57413b31518f1be68
andalex <and4alex@gmail.com>
2016-03-07 15:46:34 -0800

    updates to login/signup dir

--- app/app.js
+++ app/app.js
@@ -5,12 +5,13 @@ import satellizer   from 'satellizer';
 import main         from './components/index.html';
 import components   from './components';
 import ngDialog     from 'ng-dialog';
+import toastr     from 'angular-toastr';
 // import filters   from './filters';
 import authConfig  from './js/auth-config';
 //import configs   from './config';
 import services   from './services';
 
-const app = angular.module('myApp', [router, resource, satellizer, services, ngDialog]);
+const app = angular.module('myApp', [router, resource, satellizer, services, ngDialog, toastr]);
 
 const API_URL = 'http://localhost:3000/';
 const API_ENDPOINTS = 'api/motivate/:id';
--- app/components/index.js
+++ app/components/index.js
@@ -1,9 +1,11 @@
 import motFeedDr from './feed/mot-feed';
-import userDr from './user/mot-login';
+import loginDr from './user/mot-login';
+import signupDr from './user/mot-signup';
 /**
  * Bundle components in this dir. so that app can be passed to each.
  */
 export default function(angularModule) {
-	userDr(angularModule);
+	loginDr(angularModule);
+	signupDr(angularModule);
 	motFeedDr(angularModule);
 }
