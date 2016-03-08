e702a9d058c22e5cc140be4dc3a4adeafbddd10f
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-08 15:10:28 -0800

    add name field to project

--- app/app.js
+++ app/app.js
@@ -18,15 +18,12 @@ import './node_modules/angular-toastr/dist/angular-toastr.css';
 
 import './scss/main.scss';
 
-console.log('in app',BASE_URL);
 /**
  * App Setup:
  */
 const app = angular.module('myApp', [router, resource, satellizer, services, ngDialog, toastr]);
 
 app.constant( 'baseUrl', BASE_URL);
-const API_URL = 'http://localhost:3000/';
-const API_ENDPOINTS = 'api/v1/';
 
 components(app);
 authConfig(app);
