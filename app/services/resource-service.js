f3aed2625b5822366a68f3f767ad19f473c32125
Miles Hochstein <miles.hochstein@gmail.com>
2016-03-08 14:16:35 -0800

    all

--- app/services/resource-service.js
+++ app/services/resource-service.js
@@ -1,32 +1,5 @@
 export default function(angularModule) {
 
-<<<<<<< HEAD
-    angularModule.provider('resourceService', function() {
-        var url = '';
-        var endpoints = '';
-        
-        this.url = function(inputUrl) {
-            url = inputUrl;
-        };
-        this.endpoints = function(inputEndPoints) {
-            endpoints = inputEndPoints;
-        };
-
-        this.$get = function($resource) {
-            return $resource(url + endpoints, {
-                id: '@_id'
-            }, {
-                update: {
-                    method: 'PATCH',
-                },
-                delete: {
-                    method: 'DELETE',
-                },
-            });
-        };
-    });
-}
-=======
         function create(name, url) {
             angularModule.factory(name, ['$resource', 'baseUrl',
                 function($resource, baseUrl) {
@@ -45,4 +18,4 @@ export default function(angularModule) {
         }
         create('ProfileService', '/api/v1/user');
     }
->>>>>>> master
+
