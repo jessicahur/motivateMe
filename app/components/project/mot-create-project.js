929fc740e6369982f07a498d064cc4bb4677fa69
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-08 19:37:00 -0800

    have some kind of notification when a project was successfully posted. It definitely needs styling

--- app/components/project/mot-create-project.js
+++ app/components/project/mot-create-project.js
@@ -8,13 +8,13 @@ export default function(angularModule) {
             replace: true,
             restrict: 'E',
             template: motCreateProject,
-            controller: function($scope, ProjectService, $window, $http) {
+            controller: function($scope, ProjectService, $window) {
               $scope.project = new ProjectService();
               $scope.post = function() {
                 $scope.project.progress = $scope.project.progress.split(', ');
                 $scope.project.author = $window.localStorage.getItem('userId');
                 $scope.project.$save(res => {
-                  console.log(res);
+                  $scope.savedProject = res;
                 })
               }
             }
