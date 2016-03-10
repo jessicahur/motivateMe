65616c142c3624c26797e3eef934a0c9dd52d5c6
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-09 16:15:11 -0800

    Have comments automatically update

--- app/components/motivation/comments.js
+++ app/components/motivation/comments.js
@@ -8,41 +8,39 @@ export default function(angularModule) {
             replace: true,
             restrict: 'E',
             template: commentsTemplate,
-            scope: {},
+            scope: {
+              'authed': '=',
+              'projectId': '=',
+              'comments': '='
+            },
             controller: function($auth, $scope, $location, $window, CommentService) {
-              $scope.comment = new CommentService();
-              if ($auth.isAuthenticated()) {
-                $scope.authed = true;
-              }
-              else {
-                $scope.authed = false;
-              }
               //for rating
-                $scope.rate = 5;
-                $scope.max = 10;
-                $scope.isReadonly = false;
+                // $scope.rate = 5;
+                // $scope.max = 10;
+                // $scope.isReadonly = false;
 
-                $scope.hoveringOver = function(value) {
-                  $scope.overStar = value;
-                  $scope.percent = 100 * (value / $scope.max);
-                };
+                // $scope.hoveringOver = function(value) {
+                //   $scope.overStar = value;
+                //   $scope.percent = 100 * (value / $scope.max);
+                // };
 
-                $scope.ratingStates = [
-                  {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
-                  {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
-                  {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
-                  {stateOn: 'glyphicon-heart'},
-                  {stateOff: 'glyphicon-off'}
-                ];
+                // $scope.ratingStates = [
+                //   {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+                //   {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+                //   {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+                //   {stateOn: 'glyphicon-heart'},
+                //   {stateOff: 'glyphicon-off'}
+                // ];
+              $scope.comment = new CommentService();
+              console.log('comment service', $scope.comment);
               $scope.post = function () {
-                var projectId = '56df73f5ef7b9c5d07f5978f';//$location.path; Will fix this once Miles has individual project view on
-                $scope.comment.project = projectId;
+                $scope.comment.project = $scope.projectId;
                 $scope.comment.author = $window.localStorage.getItem('userId');
                 $scope.comment.$save(savedCmt => {
-                  console.log('Saved Comment',savedCmt);
+                  $scope.comments.push(savedCmt);
+                  console.log(savedCmt);
                 });
-
-
+                $scope.comment = new CommentService();
                 // console.log($location);
                 // console.log($window.localStorage.getItem('userId'));
                 // console.log($scope.comment);
--- app/services/resource-service.js
+++ app/services/resource-service.js
@@ -3,7 +3,7 @@ export default function(angularModule) {
         function create(name, url) {
             angularModule.factory(name, ['$resource', 'baseUrl',
                 function($resource, baseUrl) {
-                    return $resource(`${baseUrl}${url}/:id`, {
+                    return $resource(`${baseUrl}${url}/:id?populate=author`, {
                         id: '@_id'
                     }, {
                         update: {
@@ -17,6 +17,6 @@ export default function(angularModule) {
         create('ProfileService', '/api/v1/users');
         create('ProjectService', '/api/v1/projects')
         create('FeedService', '/projects');
-         create('CommentService', '/api/v1/comments');
+        create('CommentService', '/api/v1/comments');
     };
 
