5c196362869425ab757765f52a34052777c75019
Miles Hochstein <miles.hochstein@gmail.com>
2016-03-09 16:19:53 -0800

    delete one profile ui view

--- app/components/project/project-detail.js
+++ app/components/project/project-detail.js
@@ -19,7 +19,6 @@ export default function(angularModule) {
                         ProjectService.update(param, JSON.stringify(update));
                     };
 
-
             }]
         };
     });
