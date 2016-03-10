673fde44c6252cb50fb9475e6fc5262b36e16391
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-10 14:17:33 -0800

    some changes

--- server/lib/public-router.js
+++ server/lib/public-router.js
@@ -2,31 +2,18 @@ const router = require('express').Router();
 const Projects = require('../models/Project');
 const Comments = require('../models/Comment');
 
-
-// function find (query, req, res, populateItem) {
-//   PhysicalBook.find(query)
-//               .populate(populateItem)
-//               .lean()
-//               .exec( (err, books) => {
-//                 if(err) {
-//                           return res.status(500).send(err[0]);
-//                         }
-//                 res.send(books);
-//               });
-// }
-
 //GET all projects
 router.get('/', (req, res, next) => {
   Projects.find()
       .sort( {date: 'desc'} )
       .populate('author progress')
-        .lean()
-        .exec((err, projects) => {
-            if(err) {
-              return res.status(500).send(err[0]);
-            }
-            res.send(projects);
-          });
+      .lean()
+      .exec((err, projects) => {
+          if(err) {
+            return res.status(500).send(err[0]);
+          }
+          res.send(projects);
+        });
 });
 
 router.get('/:id', (req, res, next) => {
