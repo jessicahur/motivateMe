2ecc674ce2e65a2627586cd0a1b258e623e55cc1
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-17 14:09:23 -0500

    create route for each individual project

--- app/js/route-config.js
+++ app/js/route-config.js
@@ -9,13 +9,29 @@ export default function(angularModule) {
                     requireAuth: false
                 }
             })
-            // .state('feed-detail', {
-            //     url: '/detail',
-            //     template: '<feed-detail></feed-detail>',
-            //     data: {
-            //         requireAuth: false
-            //     }
-            // })
+            .state('feed.detail', {
+                url: 'detail/:projectId',
+                template: '<feed-detail></feed-detail>',
+                data: {
+                    requireAuth: false
+                },
+                controller: function($scope, $stateParams, $auth, ProgressService, CommentService) {
+                    Object.keys($stateParams).forEach(key => {
+                        $scope[key] = $stateParams[key];
+                    });
+                    console.log($stateParams);
+                    if ($auth.isAuthenticated()) {
+                        $scope.authed = true;
+                    }
+                    else {
+                        $scope.authed = false;
+                    }
+                    $scope.selectProg = function(prog, id) {
+                        $scope.commentOn = prog;
+                        $scope.commentProgId = id;
+                    }
+                }
+            })
             .state('profile', {
               url: '/profile',
               template: '<mot-profile></mot-profile>',
